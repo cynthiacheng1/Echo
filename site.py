5c4351412b4c1f5f5cc76dd72a45af92028ad7ec
Cynthia Cheng <cynthiacheng1022@gmail.com>
2017-10-04 14:16:38 -0400

    fhjks

--- site.py
+++ site.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request
 
 site = Flask(__name__)
@@ -6,14 +7,14 @@ site = Flask(__name__)
 def root():
 	return render_template("form.html")
 
-@site.route('/response', methods=['POST', 'GET'])
-def response():
+@site.route('/result', methods=['POST', 'GET'])
+def result():
 	name = 'blah'
 	if request.method == 'POST':
 		name = request.form['name']
 	else:
 		name = request.args['name']
-	return render_template("response.html", input = name, method = request.method)
+	return render_template("result.html", input = name, method = request.method)
 
 if __name__ == '__main__':
     site.debug = True
